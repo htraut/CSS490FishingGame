31a444dc03f7f2dd0c0f5cdcb604be10eb75ea8f
htraut <herbtraut@gmail.com>
2015-11-27 18:32:13 -0800

    The Fisherman!

---
+++ public_html/src/FishingGame/Objects/Fisherman.js
@@ -0,0 +1,25 @@
+/*
+ * @auth: Herbert Traut
+ * @file: Fisherman.js
+ * @date: 11-27-15
+ * @brief: The fisherman controlled by the player character
+ */
+
+/* global GameObject, gEngine */
+
+function Fisherman(texture) {
+    this.kDelta = 0.5;
+    
+    this.mFisherman = new SpriteRenderable(texture);
+    this.mFisherman.setColor([1,1,1,0]);
+    this.mFisherman.getXform().setPosition(0,0);
+    this.mFisherman.getXform().setSize(6,13);
+    this.mFisherman.setElementPixelPositions(0, 120, 0, 180);
+    GameObject.call(this, this.mFisherman);
+}
+gEngine.Core.inheritPrototype(Fisherman, GameObject);
+
+Fisherman.prototype.update = function(){
+    
+};
+
