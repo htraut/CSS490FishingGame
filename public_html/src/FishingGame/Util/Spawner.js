b4757687b1bc27c8c5f3b9d09c0d697a882fc1bd
htraut <herbtraut@gmail.com>
2015-11-27 21:50:02 -0800

    Spawner added

---
+++ public_html/src/FishingGame/Util/Spawner.js
@@ -0,0 +1,77 @@
+/*
+ * @auth: Herbert Traut
+ * @file: Spawner.js
+ * @date: 11-27-15
+ * @brief: Spawn controller for objects
+ * 
+ */
+
+
+/* global Camera */
+
+function Spawner(camera){
+    this.mLocation = camera.getWCCenter();
+    this.mWidth = camera.getWCWidth();
+    this.mHeight = camera.getWCHeight();
+};
+
+/*
+ * @amount: the amount to spawn
+ * @type: the object type to be spawned
+ */
+Spawner.prototype.populate = function (amount, type, renderableObj){
+    
+    if(amount === 0) return;
+    if(type === null) return;
+    
+    var population = [];
+    var i = 0;
+    var objXform = null;
+    var object = null;
+    var w, h, x, y;
+    
+    for(i; i < amount; i++){
+        switch(type){
+            case "Fish":
+                object = new Fish(renderableObj);
+                objXform = object.getXform();
+                w = Math.floor((Math.random()*10) + 1);
+                h = Math.floor((Math.random()*10) + 1);
+                objXform.setSize(w, h);
+                object.setScore(w * h);
+                x = this._generateXPos();
+                y = this._generatYPos();
+                objXform.setPosition(x, y);
+                population.push(object);
+                continue;
+            default: 
+                break;
+        }
+    }
+    
+    return population;
+};
+
+Spawner.prototype._generateXPos = function(){
+    var dir = Math.round(Math.random());
+    var x;
+    var disp = Math.random();
+    if(dir === 1){
+        x = (this.mLocation[0] + (this.mWidth/2))*disp;
+    }else{
+         x = (this.mLocation[0] - (this.mWidth/2))*disp;
+    }
+    return x;
+};
+
+Spawner.prototype._generateYPos = function(){
+    var dir = Math.round(Math.random());
+    var y;
+    var disp = Math.random();
+    if(dir === 1){
+        y = (this.mLocation[0] + (this.mHeight/2))*disp;
+    }else{
+        y = (this.mLocation[0] - (this.mHeight/2))*disp;
+    }
+    return y;
+};
