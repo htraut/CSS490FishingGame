4d6ff802461053e3d01b2a732ee6e8b0abcf8465
joeyguinasso <joeyguinasso@yahoo.com>
2015-11-28 19:01:49 -0800

    Pan Camera with boat

---
+++ public_html/src/FishingGame/Objects/Fish SubClasses/Shark.js
@@ -0,0 +1,37 @@
+/*
+ * @auth: Herbert Traut
+ * @file: Shark.js
+ * @date: 11-28-15
+ * @brief: Shark which chases fishing hook when the hook is nearby
+ */
+
+
+/* global GameObject, gEngine, Fish */
+
+'use strict';
+
+function Shark(texture){
+    
+    Fish.call(this, texture);
+    this.mFish.setElementPixelPositions(35, 100, 250, 270);
+    this.mChaseDist = 10;
+    this.mRotateRate = 1;
+    this.mSpeed = 1.5;
+}
+gEngine.Core.inheritPrototype(Shark, Fish);
+
+Shark.eStatus = Object.freeze({
+    eChase: 8
+});
+
+Shark.prototype.update = function (){
+    if(this.mStatus === Fish.eStatus.eCollideRight){
+        this.mSpeed *= -1;
+        this.mStatus = 0;
+    }else if(this.mStatus === Fish.eStatus.eCollideLeft){
+        this.mSpeed *= -1;
+        this.mStatus = 0;
+    }
+    this.mRenderComponent.getXform().incXPosBy(this.mSpeed);
+};
+
